e373edcb6d9aa7a8a79031dfe05e31e0d058a846
Pierre LAURENT <PierreLrt72@gmail.com>
2020-03-24 11:24:17 +0100

    Modification du design des formulaires

--- resources/views/actualites/create.blade.php
+++ resources/views/actualites/create.blade.php
@@ -2,20 +2,28 @@
 
 @section('content')
     <div class="container">
-        <form action="{{ action('ActualiteController@store') }}" class="forms-sample" method="POST">
-            <input name="_token" type="hidden" value="{{ csrf_token() }}" />
-
-            <div class="form-group">
-                <label>Titre</label>
-                <input type="text" class="form-control" name="titre" id="titre">
+        <div class="card mb-2">
+            <div class="card-header">
+                Création d'une actualité
             </div>
 
-            <div class="form-group">
-                <label>Contenu</label>
-                <textarea class="form-control" rows="6" name="contenu" id="contenu"></textarea>
-            </div>
+            <div class="card-body">
+                <form action="{{ action('ActualiteController@store') }}" class="forms-sample" method="POST">
+                    <input name="_token" type="hidden" value="{{ csrf_token() }}" />
 
-            <button type="submit" class="btn btn-primary">Envoyer</button>
-        </form>
+                    <div class="form-group">
+                        <label>Titre</label>
+                        <input type="text" class="form-control" name="titre" id="titre">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Contenu</label>
+                        <textarea class="form-control" rows="6" name="contenu" id="contenu"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                </form>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/evenements/edit.blade.php
+++ resources/views/evenements/edit.blade.php
@@ -2,35 +2,43 @@
 
 @section('content')
     <div class="container">
-        <form action="{{ action('EvenementController@update', $evenement['id']) }}" class="forms-sample" method="POST">
-            <input name="_token" type="hidden" value="{{ csrf_token() }}" />
-
-            <div class="form-group">
-                <label>Intitulé</label>
-                <input type="text" class="form-control" name="intitule" id="intitule" value="{{$evenement['intitule']}}">
+        <div class="card mb-2">
+            <div class="card-header">
+                Edition d'un évenement
             </div>
 
-            <div class="form-group">
-                <label>Description</label>
-                <textarea class="form-control" rows="6" name="description" id="description">{{$evenement['description']}}</textarea>
-            </div>
+            <div class="card-body">
+                <form action="{{ action('EvenementController@update', $evenement['id']) }}" class="forms-sample" method="POST">
+                    <input name="_token" type="hidden" value="{{ csrf_token() }}" />
 
-            <div class="form-group">
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="date_debut">Date de début</label>
-                        <input type="date" name="date_debut" id="date_debut" value="{{$evenement['date_debut']}}" class="form-control">
+                    <div class="form-group">
+                        <label>Intitulé</label>
+                        <input type="text" class="form-control" name="intitule" id="intitule" value="{{$evenement['intitule']}}">
                     </div>
 
-                    <div class="col-md-6">
-                        <label for="date_fin">Date de fin</label>
-                        <input type="date" name="date_fin" id="date_fin"  value="{{$evenement['date_fin']}}" class="form-control">
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea class="form-control" rows="6" name="description" id="description">{{$evenement['description']}}</textarea>
                     </div>
-                </div>
 
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="date_debut">Date de début</label>
+                                <input type="date" name="date_debut" id="date_debut" value="{{$evenement['date_debut']}}" class="form-control">
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="date_fin">Date de fin</label>
+                                <input type="date" name="date_fin" id="date_fin"  value="{{$evenement['date_fin']}}" class="form-control">
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Editer</button>
+                </form>
             </div>
-
-            <button type="submit" class="btn btn-primary">Editer</button>
-        </form>
+        </div>
     </div>
 @endsection
